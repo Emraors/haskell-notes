c5d18998288776b126ec82309a72a54067dca64d
Emraors <eros.martinelli@hotmail.it>
2023-05-28 16:24:40 +0200

    Refactoring

--- lambdaCalculus/ReaderMonad.hs
+++ lambdaCalculus/ReaderMonad.hs
@@ -1,28 +1,26 @@
 module ReaderMonad () where
 
 import Control.Monad.Reader
+import Data.Functor
+
 
 type VarName = String
 
 type Vars = [VarName]
 
-data Term = Var VarName
-    | Lambda VarName Term
-    | App Term Term deriving (Eq, Show)
+data Term
+  = Var VarName
+  | Lambda VarName Term
+  | App Term Term
+  deriving (Eq, Show)
 
 type Env = [(VarName, Term)]
 
 type EvalM a = Reader Env a
 
 eval :: Term -> EvalM Term
-eval (Var x) = do
-  env <- ask
-  case lookup x env of
-    Just v -> return v
-    Nothing -> return (Var x)
-eval (Lambda x body) = do
-  env <- ask
-  return $ Lambda x (runReader (eval body) ((x, Var x) : env))
+eval (Var x) = ask >>= maybe (return $ Var x) return . lookup x
+eval (Lambda x body) = ask <&> (Lambda x . runReader (eval body) . ((x, Var x) :))
 eval (App func arg) = do
   func' <- eval func
   arg' <- eval arg
@@ -36,8 +34,6 @@ initialEnv = []
 evalExpr :: Term -> Term
 evalExpr expr = runReader (eval expr) initialEnv
 
-
-
 and' :: Term
 and' = Lambda "x" (Lambda "y" (App (App (Var "x") (Var "y")) (Var "x")))
 
@@ -47,7 +43,6 @@ true' = Lambda "x" (Lambda "y" (Var "x"))
 false' :: Term
 false' = Lambda "x" (Lambda "y" (Var "y"))
 
-
 zero' :: Term
 zero' = Lambda "f" (Lambda "x" (Var "x"))
 
@@ -56,7 +51,6 @@ one'' = Lambda "f" (Lambda "x" (App (Var "f") (Var "x")))
 
 succ' :: Term
 succ' = Lambda "n" (Lambda "g" (Lambda "y" (App (Var "g") (App (App (Var "n") (Var "g")) (Var "y")))))
-
 
 one' :: Term
 one' = App succ' zero'
